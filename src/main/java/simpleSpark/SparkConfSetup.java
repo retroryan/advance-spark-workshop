b2fe9723b539fa14e0544639ac89750cfbca194b
Ryan Knight <ryan@grandcloud.com>
2015-07-01 06:54:45 -0600

    Cleanup Spark Conf Setup to remove uneeded parameters.

--- src/main/java/simpleSpark/SparkConfSetup.java
+++ src/main/java/simpleSpark/SparkConfSetup.java
@@ -7,19 +7,10 @@ import org.apache.spark.api.java.JavaSparkContext;
 
 public interface SparkConfSetup {
 
-    //DSE Host is the IP of the Spark Master started by DSE
-    String DSE_HOST = "localhost";
-
-    //Driver Host is the ip of the application running the spark application
-    //String DRIVER_HOST = "127.0.0.1";
-
     static public SparkConf getSparkConf() {
 
         return new SparkConf()
-                .setAppName("SimpleSpark")
-                .set("spark.cassandra.connection.host",DSE_HOST);
-                //.set("spark.driver.host", DRIVER_HOST)
-                //.setMaster("spark://" + DSE_HOST + ":7077");
+                .setAppName("SimpleSpark");
     }
 
     static public JavaSparkContext getJavaSparkContext() {
